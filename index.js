b3cd9dd3af7559d5e95e0f7362084ad703f5a410
Saúl Gutiérrez <62368834+saulgutierrez@users.noreply.github.com>
2025-06-23 22:36:07 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Configuracion Swiper */
+/* -- Swiper Inicio -- */
+/* -- Swiper Testimonios -- */
+/* Funcion para agregar/quitar la clase active al header cuando se haga scroll */
+/* ScrollReveal */
+/* Mostrar/ocultar menu */
+// Con esta funcion cambiaremos el icono del boton del menu cuando hagamos cliek en el
+// Al hacer click en el boton del menu, se colocara/quitara la clase activa y llamamos a la funcion cambiarIcono()
+// Con esta funcion, se ocultara el menu cuando se haga click en alguno de los enlaces del menu
